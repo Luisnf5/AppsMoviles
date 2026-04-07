fa3df040901c01ab16d68a11cf1737cb6c54d05a
javier <javier.fuentesp@estudiante.uam.es>
2026-04-07 20:12:58 +0200

    feat: añade activity contenedora y navegación base

--- app/src/main/java/com/example/tragomaestro/MainActivity.kt
+++ app/src/main/java/com/example/tragomaestro/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.tragomaestro
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tragomaestro.databinding.ActivityMainBinding
 
-// Lo dejamos como la Playgrouhd
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
 }
