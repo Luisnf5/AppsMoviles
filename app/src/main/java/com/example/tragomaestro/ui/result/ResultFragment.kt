1b0f377e27e9eca41220923f46013ed9be32534c
javier <javier.fuentesp@estudiante.uam.es>
2026-04-07 20:46:17 +0200

    feat: añadiendo fragmentos base del paquete ui (errores se van al crear los archvios xml)

---
+++ app/src/main/java/com/example/tragomaestro/ui/result/ResultFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tragomaestro.ui.result
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tragomaestro.R
+
+class ResultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Error: Se arregla cuando añadas el xml
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+}
