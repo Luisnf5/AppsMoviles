0377015aba7446cf6474abbf70258942aee36463
javier <javier.fuentesp@estudiante.uam.es>
2026-05-05 20:56:15 +0200

    refactor: activamos la base de datos en creación modo "lazy"

--- app/src/main/java/com/example/tragomaestro/TragoMaestroApplication.kt
+++ app/src/main/java/com/example/tragomaestro/TragoMaestroApplication.kt
@@ -1,9 +1,15 @@
 package com.example.tragomaestro
 
 import android.app.Application
+import com.example.tragomaestro.database.TragoMaestroDatabase
 import timber.log.Timber
 
 class TragoMaestroApplication : Application() {
+
+    val database: TragoMaestroDatabase by lazy {
+        TragoMaestroDatabase.getDatabase(this)
+    }
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
