8bb79e5b6d660edea4afd3089cdebe72375fb8ab
luisnf5 <luisnun.fdez@gmail.com>
2026-05-05 21:33:52 +0200

    feat: expone base de datos Room desde Application

--- app/src/main/java/com/example/tragomaestro/TragoMaestroApplication.kt
+++ app/src/main/java/com/example/tragomaestro/TragoMaestroApplication.kt
@@ -1,12 +1,18 @@
 package com.example.tragomaestro
 
 import android.app.Application
+import com.example.tragomaestro.database.TragoMaestroDatabase
 import timber.log.Timber
 
 class TragoMaestroApplication : Application() {
+
+    val database: TragoMaestroDatabase by lazy {
+        TragoMaestroDatabase.getDatabase(this)
+    }
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        Timber.i("Timber inicializado en TragoMaestroApplication")
+        Timber.i("TragoMaestroApplication inicializada")
     }
 }
